64dff6d149a3f780a29ba4f215dfbca7adfbcfa2
Kyle Carter <kyc@trustpilot.com>
2020-10-21 19:56:42 -0600

    init

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+app.use( express.json() );
+app.post( '/', ( req, res ) => {
+    console.log( 'received webhook', req.body );
+    res.sendStatus(200);
+} );
+app.listen( port, () => console.log( 'Node.js server started on port' + port ) );
